d77b8faa0e3a85c1f2516731c2ae277c1ea3e4f5
prasanthmp500 <prasanthmp500@gmail.com>
2020-12-27 18:21:23 +0000

    Zuul implementation reverse proxy

---
+++ spring-cloud-routing-and-filtering-zuul/src/test/java/com/example/routingandfiltering/RoutingandfilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routingandfiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingandfilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
